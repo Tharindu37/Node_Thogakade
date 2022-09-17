314bec2fc82e377229f3030b4826151e0697ad5b
Tharindu37 <tharindut520@gmail.com>
2022-09-17 05:50:32 +0530

    Connect mongodb database

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const port = 3000;
 
 const app = express();
 
-app.listen(port,()=>{
-    console.log(`Thogakade service up and running on port number ${port}`);
+mongoose.connect('mongodb://localhost:27017/thogakade').then(()=>{
+    app.listen(port,()=>{
+        console.log(`Thogakade service up and running on port number ${port}`);
+    })
+}).catch(error=>{
+
 })
 
-app.get("/test", (req, resp)=>{
-    resp.send({'status':'Success!'})
-})
+
+
