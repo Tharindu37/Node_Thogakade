cd4187914f193ba9ea84c9619a62363c1c7df5e5
Tharindu37 <tharindut520@gmail.com>
2022-09-17 06:05:25 +0530

    Create

---
+++ controller/CustomerController.js
@@ -0,0 +1,16 @@
+
+const saveCustomer=(req,resp)=>{
+
+}
+const updateCustomer=(req,resp)=>{
+
+}
+const getCustomer=(req,resp)=>{
+
+}
+const deleteCustomer=(req,resp)=>{
+
+}
+const getAllCustomers=(req,resp)=>{
+
+}
