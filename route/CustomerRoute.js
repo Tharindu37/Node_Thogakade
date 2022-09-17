b83acf2f0fa6519c79441151159c4256a03aa7f5
Tharindu37 <tharindut520@gmail.com>
2022-09-17 07:12:54 +0530

    add update, get, delete and getAll route

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -4,5 +4,10 @@ const customerController = require('../controller/CustomerController');
 const router = express.Router();
 
 router.post('/save', customerController.saveCustomer);
+router.post('/update', customerController.updateCustomer);
+router.post('/get', customerController.getCustomer);
+router.post('/delete', customerController.deleteCustomer);
+router.post('/getAll', customerController.getAllCustomers);
+
 
 module.exports = router;
