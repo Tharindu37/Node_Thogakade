7ff75140987aa8209c7a0abcf5ed99e16e8cdf62
Tharindu37 <tharindut520@gmail.com>
2022-09-17 07:21:42 +0530

    Fix http method issues

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -4,10 +4,10 @@ const customerController = require('../controller/CustomerController');
 const router = express.Router();
 
 router.post('/save', customerController.saveCustomer);
-router.post('/update', customerController.updateCustomer);
-router.post('/get', customerController.getCustomer);
-router.post('/delete', customerController.deleteCustomer);
-router.post('/getAll', customerController.getAllCustomers);
+router.put('/update', customerController.updateCustomer);
+router.get('/get', customerController.getCustomer);
+router.delete('/delete', customerController.deleteCustomer);
+router.get('/getAll', customerController.getAllCustomers);
 
 
 module.exports = router;
