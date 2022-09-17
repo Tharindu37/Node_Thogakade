d41d304e554ae480dd681ef36a8fbe7db328a56a
Tharindu37 <tharindut520@gmail.com>
2022-09-17 06:18:09 +0530

    Create saveCustomer method

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,6 +1,17 @@
+const Customer = require('../model/CustomerSchema');
 
 const saveCustomer=(req,resp)=>{
-
+    const customerDto = new Customer({
+        id: req.body.id,
+        name: req.body.name,
+        address: req.body.address,
+        salary: req.body.salary
+    });
+    customerDto.save().then(result=>{
+        resp.status(201).json(result);
+    }).catch(error=>{
+        resp.status(500).json(error);
+    });
 }
 const updateCustomer=(req,resp)=>{
 
